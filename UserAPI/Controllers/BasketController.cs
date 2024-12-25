4082a76a182353a3bd4d06d4350f441d95908865
Yurii Salyha <yuriisalyha@gmail.com>
2024-12-25 19:08:24 +0100

    Implemented Basket service and repository

---
+++ UserAPI/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
