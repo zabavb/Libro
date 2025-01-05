0f0f48e12b00a1eac5972f621db788b01e7962da
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-01-05 21:09:54 +0200

    Add FeedbackRepository and IFeedbackRepository

---
+++ BookApi/Repositories/FeedbackRepository.cs
@@ -0,0 +1,60 @@
+﻿using BookApi.Data;
+using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Repositories
+{
+    public class FeedbackRepository : IFeedbackRepository
+    {
+        private readonly BookDbContext _context;
+
+        public FeedbackRepository(BookDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Feedback entity)
+        {
+            entity.Id = Guid.NewGuid();
+            _context.Feedbacks.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var author = await _context.Feedbacks.FirstOrDefaultAsync(a => a.Id == id);
+            if (author == null)
+            {
+                throw new KeyNotFoundException("Author not found");
+            }
+            _context.Feedbacks.Remove(author);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Feedback>> GetAllAsync()
+        {
+            return await _context.Feedbacks.ToListAsync();
+        }
+
+        public async Task<Feedback?> GetByIdAsync(Guid id)
+        {
+            return await _context.Feedbacks.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task UpdateAsync(Feedback entity)
+        {
+            var feedbackToUpdate = await _context.Feedbacks.FirstOrDefaultAsync(a => a.Id == entity.Id);
+            if (feedbackToUpdate == null)
+            {
+                throw new KeyNotFoundException("Author not found");
+            }
+
+            feedbackToUpdate.ReviewerName = entity.ReviewerName;
+            feedbackToUpdate.Comment = entity.Comment;
+            feedbackToUpdate.Rating = entity.Rating;
+            feedbackToUpdate.Date = entity.Date;
+            feedbackToUpdate.IsPurchased = entity.IsPurchased;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
