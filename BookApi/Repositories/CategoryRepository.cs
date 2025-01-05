93ccabefd7f1b1c83e0d02119c4cad7ea8cc0abf
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-01-05 21:09:36 +0200

    Add CategoryRepository and ICategoryRepository

---
+++ BookApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using BookApi.Data;
+using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly BookDbContext _context;
+        public CategoryRepository(BookDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Category entity)
+        {
+            entity.Id = Guid.NewGuid();
+            _context.Categories.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(a => a.Id == id) ?? throw new KeyNotFoundException("Category not found");
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(Guid id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task UpdateAsync(Category entity)
+        {
+            var existingCategory = await _context.Categories.FirstOrDefaultAsync(a => a.Id == entity.Id) ?? throw new KeyNotFoundException("Category  not found");
+            existingCategory.Name = entity.Name;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
