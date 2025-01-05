afd402370baa9ef2fefe61191f7d1d742ea5f275
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-01-05 21:32:29 +0200

    Refactor mapping profiles to use .ReverseMap()

--- BookApi/Profiles/AuthorProfile.cs
+++ BookApi/Profiles/AuthorProfile.cs
@@ -8,13 +8,9 @@ namespace BookApi.Profiles
         public AuthorProfile()
         {
             CreateMap<Author, AuthorDto>()
-                .ForMember(dest => dest.AuthorId, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
-                .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => src.Biography));
-
-            CreateMap<AuthorDto, Author>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore()); 
+            .ForMember(dest => dest.AuthorId, opt => opt.MapFrom(src => src.Id))
+            .ReverseMap()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
         }
     }
 }
--- BookApi/Profiles/FeedbackProfile.cs
+++ BookApi/Profiles/FeedbackProfile.cs
@@ -9,13 +9,7 @@ namespace BookApi.Profiles
         {
             CreateMap<Feedback, FeedbackDto>()
                 .ForMember(dest => dest.FeedbackId, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.ReviewerName, opt => opt.MapFrom(src => src.ReviewerName))
-                .ForMember(dest => dest.Comment, opt => opt.MapFrom(src => src.Comment))
-                .ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.Rating))
-                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Date))
-                .ForMember(dest => dest.IsPurchased, opt => opt.MapFrom(src => src.IsPurchased));
-
-            CreateMap<FeedbackDto, Feedback>()
+                .ReverseMap()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
         }
     }
