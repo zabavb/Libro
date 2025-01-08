f8f8ef8b9a5408bc15365ce766bf14704a8357cc
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-01-08 16:56:18 +0200

    Add BookFilter and BookSort classes in Library

---
+++ Library/Sortings/BookSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Sortings
+{
+    public class BookSort
+    {
+        public Bool Title { get; set; }
+        public Bool Price { get; set; }
+        public Bool Year { get; set; }
+        public Bool Language { get; set; }
+        public Bool Cover { get; set; }
+        public Bool IsAvaliable { get; set; }
+    }
+}
