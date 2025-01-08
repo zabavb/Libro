6393c107b3606963c009ae5944c2c2bc79383a28
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-01-08 16:59:26 +0200

    Add global using directives for sorting and filtering books

--- BookApi/GlobalUsings.cs
+++ BookApi/GlobalUsings.cs
@@ -3,3 +3,7 @@ global using CategoryDto = Library.DTOs.Book.Category;
 global using AuthorDto = Library.DTOs.Book.Author;
 global using PublisherDto = Library.DTOs.Book.Publisher;
 global using FeedbackDto = Library.DTOs.Book.Feedback;
+global using Sort = Library.Sortings.BookSort;
+global using Filter = Library.Filters.BookFilter;
+
+
