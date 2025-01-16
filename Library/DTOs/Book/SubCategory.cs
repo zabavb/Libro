f3b9914f382d953f4f5d2b3d436b57f09039dd6c
Bohdan <bogdandibskij@gmail.com>
2025-01-16 15:57:36 +0200

    New DTO for transferring SubCategory data between layers

---
+++ Library/DTOs/Book/SubCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.DTOs.Book
+{
+    public class SubCategory
+    {
+        public Guid SubCategoryId { get; set; }         
+        public Guid BookId { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
