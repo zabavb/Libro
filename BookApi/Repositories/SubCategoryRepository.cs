cd3ef870114f08ebc94d103333aaa57138d08c4a
Bohdan <bogdandibskij@gmail.com>
2025-01-16 15:23:23 +0200

    Implementation of ISubCategoryRepository, handling database operations for SubCategory

---
+++ BookApi/Repositories/SubCategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using BookApi.Data;
+using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Repositories
+{
+    public class SubCategoryRepository : ISubCategoryRepository
+    {
+        private readonly BookDbContext _context;
+
+        public SubCategoryRepository(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(SubCategory entity)
+        {
+            entity.Id = Guid.NewGuid();
+            _context.Subcategories.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var subCategory = await _context.Subcategories.FirstOrDefaultAsync(sc => sc.Id == id)
+                ?? throw new KeyNotFoundException("SubCategory not found");
+            _context.SubCategories.Remove(subCategory);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<SubCategory>> GetAllAsync()
+        {
+            return await _context.Subcategories
+                .Include(sc => sc.Category)
+                .Include(sc => sc.Book)   
+                .ToListAsync();
+        }
+
+        public async Task<SubCategory?> GetByIdAsync(Guid id)
+        {
+            return await _context.Subcategories
+                .Include(sc => sc.Category) 
+                .Include(sc => sc.Book)   
+                .FirstOrDefaultAsync(sc => sc.Id == id);
+        }
+
+        public async Task UpdateAsync(SubCategory entity)
+        {
+            var existingSubCategory = await _context.Subcategories.FirstOrDefaultAsync(sc => sc.Id == entity.Id)
+                ?? throw new KeyNotFoundException("SubCategory not found");
+
+            _context.Entry(existingSubCategory).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
