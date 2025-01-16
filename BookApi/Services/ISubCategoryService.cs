0a6640dfd517f222c11dc2e60b611567bc4fb286
Bohdan <bogdandibskij@gmail.com>
2025-01-16 15:24:41 +0200

    Interface for service layer logic specific to SubCategory

---
+++ BookApi/Services/ISubCategoryService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BookApi.Services
+{
+    public interface ISubCategoryService
+    {
+        Task<SubCategoryDto> CreateSubCategoryAsync(SubCategoryDto subCategoryDto);
+        Task<bool> DeleteSubCategoryAsync(Guid id);
+        Task<IEnumerable<SubCategoryDto>> GetSubCategoriesAsync();
+        Task<SubCategoryDto> GetSubCategoryByIdAsync(Guid id);
+        Task<SubCategoryDto> UpdateSubCategoryAsync(Guid id, SubCategoryDto subCategoryDto);
+    }
+}
