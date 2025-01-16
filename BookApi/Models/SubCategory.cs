45f055d7de682b39bd09fdd2de211af5e8db98d1
Bohdan <bogdandibskij@gmail.com>
2025-01-16 15:22:11 +0200

    New model representing a SubCategory, linked to Category and related to Book

---
+++ BookApi/Models/SubCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace BookApi.Models
+{
+    public class SubCategory
+    {
+        public Guid Id { get; set; } 
+        public Guid BookId { get; set; } 
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = null!;
+
+        public Book Book { get; set; } = null!;
+        public Category Category { get; set; } = null!;
+    }
+}
