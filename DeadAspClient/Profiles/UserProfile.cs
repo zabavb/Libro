42ccfe051a382fdd76825c3ae693a39cc82f242b
Viktor <bilonizkavik@gmail.com>
2025-01-17 10:47:50 +0100

    Renamed project with ASpNER MVC template from Client to DeadAspClient in directory

---
+++ DeadAspClient/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DeadAspClient.Models.UserEntities.User;
+using Library.DTOs.User;
+
+namespace DeadAspClient.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, ManageUserViewModel>();
+        }
+    }
+}
