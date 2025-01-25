b4cb0881dbf921685338110f0e966ea402501ab8
Viktor <bilonizkavik@gmail.com>
2025-01-25 15:15:00 +0100

    Added generation of Guid for new users with minor fix

--- UserAPI/Models/User.cs
+++ UserAPI/Models/User.cs
@@ -20,9 +20,10 @@ namespace UserAPI.Models
 
         public User()
         {
+            UserId = Guid.NewGuid();
             FirstName = string.Empty;
             LastName = string.Empty;
-            DateOfBirth = DateTime.Now;
+            DateOfBirth = DateTime.Today;
             Email = string.Empty;
             PhoneNumber = string.Empty;
             Role = RoleType.GUEST;
