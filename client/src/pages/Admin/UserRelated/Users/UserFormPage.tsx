fa3b4e8de397b08d82acc676246662dfa030e339
Viktor <bilonizkavik@gmail.com>
2025-01-25 15:44:44 +0100

    Implemented Container with component for UserForm

--- client/src/pages/Admin/UserRelated/Users/UserFormPage.tsx
+++ client/src/pages/Admin/UserRelated/Users/UserFormPage.tsx
@@ -1,41 +1,25 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import UserForm from "../../../../components/user/UserForm";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../state/redux/store";
+import { useParams, useNavigate } from "react-router-dom"
+import UserFormContainer from "../../../../containers/user/UserFormContainer"
 
 const UserFormPage = () => {
-  const { userId } = useParams<{ userId: string }>();
-  const navigate = useNavigate();
-  const users = useSelector((state: RootState) => state.users.data);
-  const [existingUser, setExistingUser] = useState<
-    typeof users[0] | undefined
-  >(undefined);
+  const { userId } = useParams<{ userId: string }>()
+	const navigate = useNavigate()
 
-  useEffect(() => {
-    if (userId) {
-      const user = users.find((u) => u.id === userId);
-      if (user) {
-        setExistingUser(user);
-      }
-    }
-  }, [userId, users]);
+	const handleGoBack = () => {
+		navigate("/admin/users")
+	}
 
-  const handleGoBack = () => {
-    navigate("/admin/users");
-  };
+	return (
+		<div>
+			<header>
+				<h1>{userId ? "Edit User" : "Add User"}</h1>
+				<button onClick={handleGoBack}>Back to User List</button>
+			</header>
+			<main>
+				<UserFormContainer userId={userId ?? ""} />
+			</main>
+		</div>
+	)
+}
 
-  return (
-    <div>
-      <header>
-        <h1>{userId ? "Edit User" : "Add User"}</h1>
-        <button onClick={handleGoBack}>Back to User List</button>
-      </header>
-      <main>
-        <UserForm existingUser={existingUser} />
-      </main>
-    </div>
-  );
-};
-
-export default UserFormPage;
+export default UserFormPage
