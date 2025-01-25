2b08e1d100066e2cc48b811f8fbef4e12a4cab41
Viktor <bilonizkavik@gmail.com>
2025-01-25 15:11:37 +0100

    Dismissed cors errors

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -91,7 +91,21 @@ Log.Logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(Log.Logger);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:58482")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
+
 
 if (app.Environment.IsDevelopment())
 {
