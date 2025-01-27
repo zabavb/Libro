f316382faea2d45ccb1c77ec17a2a905cca03595
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-01-27 16:52:32 +0200

    Updated Profiles, reverse mapping

--- OrderApi/Profiles/OrderProfile.cs
+++ OrderApi/Profiles/OrderProfile.cs
@@ -8,7 +8,9 @@ namespace OrderApi.Profiles
         public OrderProfile()
         {
             CreateMap<Order, OrderDto>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.OrderId)).ReverseMap();
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.OrderId))
+                .ReverseMap()
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id));
         }
     }
 }
