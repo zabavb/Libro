d5c92e7becc4636e20d0c6988b0dc726065903dd
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-01-27 16:58:10 +0200

    Removed unnecessary data annotations

--- OrderApi/Models/DeliveryType.cs
+++ OrderApi/Models/DeliveryType.cs
@@ -1,14 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
 
 namespace OrderApi.Models
 {
     public class DeliveryType
     {
-        [Required]
-        [Key]
+        //[Required]
+        //[Key]
         public Guid DeliveryId { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(50, ErrorMessage = "Service name should be less than 50 characters.")]
+        //[Required(AllowEmptyStrings = false)]
+        //[StringLength(50, ErrorMessage = "Service name should be less than 50 characters.")]
         public string ServiceName { get; set; }
 
         public DeliveryType()
--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,43 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 using OrderStatus = Library.DTOs.Order.OrderStatus;
 
 namespace OrderApi.Models
 {
     public class Order
     {
-        [Required]
-        [Key]
+        //[Required]
+        //[Key]
         public Guid OrderId { get; set; }
-        [Required]
+        //[Required]
         public Guid UserId { get; set; }
-        [Required]
+        //[Required]
         public List<Guid> BookIds { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(100, ErrorMessage = "Region must be less than 100 characters.")]
+        //[Required(AllowEmptyStrings = false)]
+        //[StringLength(100, ErrorMessage = "Region must be less than 100 characters.")]
         public string Region { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(100, ErrorMessage = "City must be less than 100 characters.")]
+        //[Required(AllowEmptyStrings = false)]
+        //[StringLength(100, ErrorMessage = "City must be less than 100 characters.")]
         public string City { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(255, ErrorMessage = "Address must be less than 255 characters.")]
+        //[Required(AllowEmptyStrings = false)]
+        //[StringLength(255, ErrorMessage = "Address must be less than 255 characters.")]
         public string Address { get; set; }
 
-        [Required]
-        [Range(0, float.MaxValue)]
+        //[Required]
+        //[Range(0, float.MaxValue)]
         public float Price { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(DeliveryType))]
+        //[Required]
+        //[ForeignKey(nameof(DeliveryType))]
         public Guid DeliveryTypeId { get; set; }
 
-        [Required]
-        [Range(0, float.MaxValue)]
+        //[Required]
+        //[Range(0, float.MaxValue)]
         public float DeliveryPrice { get; set; }
 
         public DateTime OrderDate { get; set; }
-        [Required]
+        //[Required]
         public DateTime DeliveryDate { get; set; }
 
         public OrderStatus Status { get; set; }
