41ed8b06c1a13cc0d604102e24da9864a24b1524
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-01-28 20:08:32 +0200

    Project setup

---
+++ OrderAPI.Tests/Repositories/OrderRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrderApi.Data;
+using OrderApi.Repository;
+using StackExchange.Redis;
+
+namespace OrderAPI.Tests.Repositories
+{
+    public class OrderRepositoryTests
+    {
+        private readonly OrderDbContext _dbContext;
+        private readonly Mock<OrderDbContext> _contextMock;
+        private readonly Mock<IConnectionMultiplexer> _redisMock;
+        private readonly Mock<IDatabase> _redisDatabaseMock;
+        private readonly Mock<ILogger<IOrderRepository>> _loggerMock;
+        private readonly OrderRepository _repository;
+
+        public OrderRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<OrderDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _dbContext = new OrderDbContext(options);
+
+            _contextMock = new Mock<OrderDbContext>();
+            _redisMock = new Mock<IConnectionMultiplexer>();
+            _redisDatabaseMock = new Mock<IDatabase>();
+            _loggerMock = new Mock<ILogger<IOrderRepository>>();
+
+            _redisMock.Setup(r => r.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(_redisDatabaseMock.Object);
+
+            _repository = new OrderRepository(_dbContext, _redisMock.Object, _loggerMock.Object);
+        }
+    }
+}
---
+++ OrderAPI.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrderApi.Services;
+
+
+namespace OrderAPI.Tests.Services
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IOrderRepository> _repositoryMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly Mock<ILogger<OrderService>> _loggerMock;
+        private readonly OrderService _orderService;
+
+        public OrderServiceTests()
+        {
+            _repositoryMock = new Mock<IOrderRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _loggerMock = new Mock<ILogger<OrderService>>();
+            _orderService = new OrderService(_repositoryMock.Object, _mapperMock.Object, _loggerMock.Object);
+        }
+    }
+}
