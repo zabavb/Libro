5dd8c8bdca60a5bd8856fe9e48d1d78ca9fe24b3
Viktor <bilonizkavik@gmail.com>
2025-01-30 11:12:26 +0100

    Refactored PagonatedResult in Library, from frontend point of view

--- Library/Extensions/PaginatedResult.cs
+++ Library/Extensions/PaginatedResult.cs
@@ -3,13 +3,9 @@
     public class PaginatedResult<T>
     {
         public ICollection<T> Items { get; set; } = new List<T>();
-        public int TotalCount { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
-
+        public int TotalCount { get; set; }
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-
-        public bool HasPreviousPage => PageNumber > 1;
-        public bool HasNextPage => PageNumber < TotalPages;
     }
 }
