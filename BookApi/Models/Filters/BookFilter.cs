53af2912eb5609603ab048b08a6188fd8e283a78
Bohdan <bogdandibskij@gmail.com>
2025-01-31 17:33:19 +0200

    Added book filtering model

---
+++ BookApi/Models/Filters/BookFilter.cs
@@ -0,0 +1,18 @@
+﻿using BookApi.Models;
+
+namespace BookAPI.Models.Filters
+{
+    public class BookFilter
+    {
+        public string? Title { get; set; }
+        public Guid? AuthorId { get; set; }
+        public float? MinPrice { get; set; }
+        public float? MaxPrice { get; set; }
+        public DateTime? MinYear { get; set; }
+        public DateTime? MaxYear { get; set; }
+        public Language? Language { get; set; }
+        public CoverType? Cover { get; set; }
+        public bool? IsAvaliable { get; set; }
+        public Guid? CategoryId { get; set; }
+    }
+}
