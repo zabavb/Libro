5d486f6b78d17c4a49b6d21327e17c9d8b832e51
Bohdan <bogdandibskij@gmail.com>
2025-01-31 17:33:41 +0200

    Added book sorting model

---
+++ BookApi/Models/Sortings/BookSort.cs
@@ -0,0 +1,12 @@
+﻿namespace BookAPI.Models.Sortings
+{
+    public class BookSort
+    {
+        public Bool Title { get; set; }
+        public Bool Price { get; set; }
+        public Bool Year { get; set; }
+        public Bool Language { get; set; }
+        public Bool Cover { get; set; }
+        public Bool IsAvaliable { get; set; }
+    }
+}
