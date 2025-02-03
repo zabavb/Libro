42421cac15307456ed9ce8239319647de93f849a
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-03 18:17:21 +0200

    Created OrderSort.cs in library

---
+++ Library/Sortings/OrderSort.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Sortings
+{
+    public class OrderSort
+    {
+        public Bool OrderDate { get; set; } // OrderDate Variable
+        public Bool BooksAmount { get; set; } // Count of BookIds list
+        public Bool OrderPrice { get; set; }
+        public Bool DeliveryPrice { get; set; }
+        public Bool DeliveryDate { get; set; }
+        public Bool Status { get; set; } 
+    }
+}
