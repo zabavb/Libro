1068c7ad8244e805a3c74cba2721e12a94cdcb0a
Bohdan <bogdandibskij@gmail.com>
2025-02-04 23:11:41 +0200

    Add usings

--- BookApi/GlobalUsings.cs
+++ BookApi/GlobalUsings.cs
@@ -4,5 +4,6 @@ global using AuthorDto = Library.DTOs.Book.Author;
 global using PublisherDto = Library.DTOs.Book.Publisher;
 global using FeedbackDto = Library.DTOs.Book.Feedback;
 global using SubCategoryDto = Library.DTOs.Book.SubCategory;
-
+global using Sort = BookAPI.Models.Sortings.BookSort;
+global using Filter = BookAPI.Models.Filters.BookFilter;
 
