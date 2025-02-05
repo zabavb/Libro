394ef89453d0e40038adffc4e74befc82bee50a4
Viktor <bilonizkavik@gmail.com>
2025-02-05 12:23:24 +0100

    Implemented notification: - Type - Added to store - Slice - Container - Component

--- client/src/state/redux/store.ts
+++ client/src/state/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slices/userSlice";
+import userReducer from "./slices/userSlice"
+import notificationReducer from "./slices/notificationSlice"
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+		notifications: notificationReducer,
   },
 });
 
