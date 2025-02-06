f1d0692d54f670d9f3eea1f4fdb658ca673f1799
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-06 15:20:30 +0200

    apiConfig setup and orderRepository created

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,9 +1,15 @@
+// ============ USER ===========
+
 export type { UserView } from "./objects/UserView"
 export type { User } from "./objects/User"
 
 export type { UserFilter } from "./Filters/UserFilter"
 
-export type { UserSort } from "./sortings/UserSort"
+export type { UserSort } from "./Sortings/UserSort"
+
+// ============ ORDER ===========
+
+export type { Order } from "./objects/Order"
 
 export type { PaginatedResponse } from "./subObjects/PaginatedResponse"
 export { Role } from "./subObjects/Role"
