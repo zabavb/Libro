754690a6e76e84542d88d68a32624d856cd39d69
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-06 16:03:28 +0200

    Removed unnecessary data annotations

--- Library/DTOs/Order/Order.cs
+++ Library/DTOs/Order/Order.cs
@@ -6,41 +6,28 @@ namespace Library.DTOs.Order
 {
     public class Order
     {
-        [Required]
-        [Key]
         public Guid Id { get; set; }
-        [Required]
+
         public Guid UserId { get; set; }
-        [Required]
+
         public List<Guid> BookIds { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(100, ErrorMessage = "Region must be less than 100 characters.")]
         public string Region { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(100, ErrorMessage = "City must be less than 100 characters.")]
+
         public string City { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(255, ErrorMessage = "Address must be less than 255 characters.")]
+
         public string Address { get; set; }
 
-        [Required]
-        [Range(0, float.MaxValue)]
         public float Price { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(DeliveryType))]
         public Guid DeliveryTypeId { get; set; }
 
-        [Required]
-        [Range(0, float.MaxValue)]
         public float DeliveryPrice { get; set; }
 
         public DateTime OrderDate { get; set; }
-        [Required]
+
         public DateTime DeliveryDate { get; set; }
 
-        [EnumRange(typeof(OrderStatus))]
         public OrderStatus Status { get; set; }
 
         public Order()
