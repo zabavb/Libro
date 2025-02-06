d05040ad80289d001319ba6070e91d0df53cf5af
Bohdan <bogdandibskij@gmail.com>
2025-02-06 22:07:06 +0200

    List for many-to-many

--- BookApi/Models/SubCategory.cs
+++ BookApi/Models/SubCategory.cs
@@ -3,17 +3,17 @@
     public class SubCategory
     {
         public Guid Id { get; set; } 
-        public Guid BookId { get; set; } 
         public Guid CategoryId { get; set; }
         public string Name { get; set; } = null!;
-
-        public Book Book { get; set; } = null!;
         public Category Category { get; set; } = null!;
+
+        public List<Book> Books { get; set; } = new();
+
 
         public SubCategory()
         {
             Name = string.Empty;
-            Book = null!;
+            Books = new List<Book>();
             Category = null!;
         }
     }
