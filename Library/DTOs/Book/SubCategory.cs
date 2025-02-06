ed85937090803d4aece3c9a6e36a2f34494dbd71
Bohdan <bogdandibskij@gmail.com>
2025-02-06 22:13:05 +0200

    Adding a List of Book IDs

--- Library/DTOs/Book/SubCategory.cs
+++ Library/DTOs/Book/SubCategory.cs
@@ -3,8 +3,9 @@
     public class SubCategory
     {
         public Guid SubCategoryId { get; set; }         
-        public Guid BookId { get; set; }
         public Guid CategoryId { get; set; }
         public string Name { get; set; }
+        public List<Guid> BookIds { get; set; } = new List<Guid>();
+
     }
 }
