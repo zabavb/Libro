c9f10ea1a6f88720bd5571f4bfc546a95e11d7cc
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-07 15:04:35 +0200

    Created order slice

--- client/src/state/redux/store.ts
+++ client/src/state/redux/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-
+import orderReducer from "./slices/orderSlice"
 const store = configureStore({
   reducer: {
     users: userReducer,
+    orders: orderReducer,
   },
 });
 
