03a21f12a811073760b3fa03f6aab2bb2a1285f3
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-07 17:54:39 +0200

    Updated cors port

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -18,9 +18,9 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 {
     var options = new ConfigurationOptions
     {
-        EndPoints = { { "redis-11440.c251.east-us-mz.azure.redns.redis-cloud.com", 11440 } },
-        User = "default",
-        Password = "QJqKikr9nj2UWhNtbsHYiuLaP3wfpTjM"
+        EndPoints = { "your_redis_endpoint", "your_port" },
+        User = "your_username",
+        Password = "your_password"
     };
     return ConnectionMultiplexer.Connect(options);
 });
@@ -76,6 +76,7 @@ builder.Services.AddCors(options =>
               .AllowCredentials();
     });
 });
+
 var app = builder.Build();
 
 app.UseCors("AllowReactApp");
