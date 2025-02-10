e49aad7f8d302954c0ddb1e289d13b9c6ee075f1
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-10 15:43:49 +0200

    Created required types - Status, Order's View Sort and Filter

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,16 +1,21 @@
 // Main types
 export type { User } from "./objects/User"
 export type { Order } from "./objects/Order"
+
 // View types
 export type { UserView } from "./objects/UserView"
+export type { OrderView } from "./objects/OrderView"
 
 // Filters
 export type { UserFilter } from "./filters/UserFilter"
+export type { OrderFilter } from "./filters/OrderFilter"
 
 // Sorts
 export type { UserSort } from "./sortings/UserSort"
+export type { OrderSort } from "./sortings/OrderSort"
 
 // Subtypes
 export type { PaginatedResponse } from "./subTypes/PaginatedResponse"
 export { Role } from "./subTypes/Role"
+export { Status } from "./subTypes/Status"
 export type { Notification } from "./subTypes/Notification"
