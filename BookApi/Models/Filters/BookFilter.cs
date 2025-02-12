abd518b0683bf66cc9fcc13ebae98ce0d6e5c6ef
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-02-12 15:15:27 +0200

    Introduce Filters classes in implementing IFilter<Author>

--- BookApi/Models/Filters/BookFilter.cs
+++ BookApi/Models/Filters/BookFilter.cs
@@ -1,8 +1,9 @@
-﻿using BookApi.Models;
+﻿using Amazon.S3.Model;
+using BookApi.Models;
 
 namespace BookAPI.Models.Filters
 {
-    public class BookFilter
+    public class BookFilter : IFilter<Book>
     {
         public string? Title { get; set; }
         public Guid? AuthorId { get; set; }
@@ -12,7 +13,43 @@ namespace BookAPI.Models.Filters
         public DateTime? MaxYear { get; set; }
         public Language? Language { get; set; }
         public CoverType? Cover { get; set; }
-        public bool? IsAvaliable { get; set; }
+        public bool IsAvaliable { get; set; }
         public Guid? CategoryId { get; set; }
+
+        public IQueryable<Book> Apply(IQueryable<Book> books)
+        {
+            if (!string.IsNullOrWhiteSpace(Title))
+                books = books.Where(b => b.Title.ToLower().Contains(Title.ToLower()));
+
+            if (AuthorId.HasValue)
+                books = books.Where(b => b.AuthorId == AuthorId.Value);
+
+            if (MinPrice.HasValue)
+                books = books.Where(b => b.Price >= MinPrice.Value);
+
+            if (MaxPrice.HasValue)
+                books = books.Where(b => b.Price <= MaxPrice.Value);
+
+            if (MinYear.HasValue)
+                books = books.Where(b => b.Year >= MinYear.Value);
+
+            if (MaxYear.HasValue)
+                books = books.Where(b => b.Year <= MaxYear.Value);
+
+            if (Language.HasValue)
+                books = books.Where(b => b.Language.ToString() == Language.Value.ToString());
+
+            if (Cover.HasValue)
+                books = books.Where(b => b.Cover.ToString() == Cover.Value.ToString());
+
+            if (IsAvaliable)
+                books = books.Where(b => b.IsAvaliable == IsAvaliable);
+
+            if (CategoryId.HasValue)
+                books = books.Where(b => b.CategoryId == CategoryId.Value);
+
+            return books;
+        }
+      
     }
 }
---
+++ BookApi/Models/Filters/SubCategoryFilter.cs
@@ -0,0 +1,20 @@
+﻿using BookApi.Models;
+
+namespace BookAPI.Models.Filters
+{
+    public class SubCategoryFilter : IFilter<SubCategory>
+    {
+        public Guid? CategoryId { get; set; }
+
+        public IQueryable<SubCategory> Apply(IQueryable<SubCategory> query)
+        {
+            if (CategoryId.HasValue)
+                query = query.Where(sc => sc.Category.Id == CategoryId.Value);
+
+            return query;
+        }
+    }
+
+
+
+}
