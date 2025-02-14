254a423866d6fcc5191cf7ce2bd9da02fc46f7df
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-02-14 20:42:33 +0200

    Created delivery type slice

--- client/src/state/redux/store.ts
+++ client/src/state/redux/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./slices/userSlice"
 import notificationReducer from "./slices/notificationSlice"
 import orderReducer from "./slices/orderSlice"
+import deliveryTypeSlice from "./slices/deliveryTypeSlice"
 const store = configureStore({
 	reducer: {
 		users: userReducer,
 		orders: orderReducer,
+		deliveryTypes: deliveryTypeSlice,
 		notifications: notificationReducer,
 	},
 	devTools: process.env.NODE_ENV !== "production",
