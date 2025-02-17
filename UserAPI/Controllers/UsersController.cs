c14d0f5c95b6df24bb612e7fbefb272b2374f84e
Viktor <bilonizkavik@gmail.com>
2025-02-17 11:27:01 +0100

    Added role-based access protection over users

--- UserAPI/Controllers/UsersController.cs
+++ UserAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using UserAPI.Services;
 
 namespace UserAPI.Controllers
@@ -36,6 +37,7 @@ namespace UserAPI.Controllers
         /// <returns>A paginated list of users.</returns>
         /// <response code="200">Returns the paginated list of users.</response>
         /// <response code="500">If an unexpected error occurs.</response>
+        [Authorize(Roles = "Admin, Moderator")]
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10, [FromQuery] string? searchTerm = null, [FromQuery] Filter? filter = null, [FromQuery] Sort? sort = null)
         {
@@ -60,6 +62,7 @@ namespace UserAPI.Controllers
         /// <response code="200">Returns the user if found.</response>
         /// <response code="404">If the user with the specified ID is not found or ID was not specified.</response>
         /// <response code="500">If an unexpected error occurs.</response>
+        [Authorize(Roles = "Admin, Moderator")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(Guid id)
         {
@@ -96,6 +99,7 @@ namespace UserAPI.Controllers
         /// <response code="201">Returns the newly created user.</response>
         /// <response code="400">If the provided user data is invalid.</response>
         /// <response code="500">If an unexpected error occurs.</response>
+        [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Create([FromForm] UserDto user)
         {
@@ -127,6 +131,7 @@ namespace UserAPI.Controllers
         /// <response code="400">If the user ID in the URL does not match the ID in the request body, or if the input is invalid.</response>
         /// <response code="404">If the user to be updated does not exist.</response>
         /// <response code="500">If an unexpected error occurs.</response>
+        [Authorize(Roles = "Admin")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(Guid id, [FromForm] UserDto user)
         {
@@ -169,6 +174,7 @@ namespace UserAPI.Controllers
         /// <response code="204">If the user is successfully deleted.</response>
         /// <response code="404">If the user to be deleted does not exist.</response>
         /// <response code="500">If an unexpected error occurs.</response>
+        [Authorize(Roles = "Admin")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id, [FromQuery] string imageUrl)
         {
