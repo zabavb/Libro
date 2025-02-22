232a9be2251310cdf2fa7f29473a8d0ce956facc
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-02-22 03:09:31 +0200

    Add book-related API routes and constants

--- client/src/api/config/apiConfig.ts
+++ client/src/api/config/apiConfig.ts
@@ -1,5 +1,6 @@
 const USER_API_BASE_URL = "https://localhost:7007/api"
 const ORDER_API_BASE_URL = "https://localhost:7051/api"
+const BOOK_API_BASE_URL = "https://localhost:7084/api"
 
 export const API_ROUTES = {
 	USERS: {
@@ -13,5 +14,11 @@ export const API_ROUTES = {
 		PAGINATED: (pageNumber: number, pageSize: number) =>
 			`${ORDER_API_BASE_URL}/orders?pageNumber=${pageNumber}&pageSize=${pageSize}`,
 		BY_ID: (id: string) => `${ORDER_API_BASE_URL}/orders/${id}`,
+	},
+	BOOKS: {
+		BASE: `${BOOK_API_BASE_URL}/books`,
+		PAGINATED: (pageNumber: number, pageSize: number) =>
+			`${BOOK_API_BASE_URL}/books?pageNumber=${pageNumber}&pageSize=${pageSize}`,
+		BY_ID: (id: string) => `${BOOK_API_BASE_URL}/books/${id}`,
 	},
 }
