7760507d4d7c408f40c2e7adc7674c16ec52dbf4
senselessthug <112238857+senselessthug@users.noreply.github.com>
2025-03-09 16:59:15 +0200

    updated userfilter

--- client/src/types/Filters/UserFilter.ts
+++ client/src/types/Filters/UserFilter.ts
@@ -1,5 +1,5 @@
 import { Role } from "../subTypes/Role"
-
+import { Subscription } from "../../types/types/order/Subscription";
 export interface UserFilter {
 	dateOfBirthStart?: string
 	dateOfBirthEnd?: string
@@ -7,3 +7,6 @@ export interface UserFilter {
 	role?: Role
 	hasSubscription?: boolean
 }
+export const filterSubscriptions = (subscriptions: Subscription[], status: string) => {
+	return subscriptions.filter((sub) => sub.status === status);
+  };
