5fc59af242a12567734d509e8221bda005b698f8
senselessthug <112238857+senselessthug@users.noreply.github.com>
2025-03-09 17:00:12 +0200

    Update Subscription

--- client/src/types/types/order/Subscription.ts
+++ client/src/types/types/order/Subscription.ts
@@ -1,11 +1,7 @@
-export type SubscriptionPlan = "basic" | "premium" | "vip";
-
 export interface Subscription {
   id: string;
-  email: string;
-  name: string;
-  plan: SubscriptionPlan;
-  startDate: string;
-  endDate: string;
-  status: "active" | "expired" | "canceled";
+  userId: string;
+  plan: "basic" | "premium" | "pro";
+  status: "active" | "inactive" | "cancelled";
+  autoRenewal: boolean;
 }
