764643ccdd6ff6c3ba01e58431b103fdff3c307d
senselessthug <112238857+senselessthug@users.noreply.github.com>
2025-03-09 16:57:17 +0200

    updated store

--- client/src/state/redux/store.ts
+++ client/src/state/redux/store.ts
@@ -1,20 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer from "./slices/userSlice"
-import bookReducer from "./slices/bookSlice"
-import notificationReducer from "./slices/notificationSlice"
-import orderReducer from "./slices/orderSlice"
-import deliveryTypeSlice from "./slices/deliveryTypeSlice"
-const store = configureStore({
-	reducer: {
-		users: userReducer,
-		orders: orderReducer,
-		deliveryTypes: deliveryTypeSlice,
-		books: bookReducer,
-		notifications: notificationReducer,
-	},
-	devTools: process.env.NODE_ENV !== "production",
-})
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+import bookReducer from "./slices/bookSlice";
+import notificationReducer from "./slices/notificationSlice";
+import orderReducer from "./slices/orderSlice";
+import deliveryTypeSlice from "./slices/deliveryTypeSlice";
+import subscriptionsReducer from "./slices/subscriptionSlice";
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export default store
+const rootReducer = {
+  users: userReducer,
+  orders: orderReducer,
+  deliveryTypes: deliveryTypeSlice,
+  books: bookReducer,
+  notifications: notificationReducer,
+  subscriptions: subscriptionsReducer,
+};
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
