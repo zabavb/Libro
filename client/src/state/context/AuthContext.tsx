0925ce2e3bb86768ce1b60f270f45290bf1dc939
zabavb <bilonizkavik@gmail.com>
2025-03-12 11:18:21 +0100

    Refactored auth's service and context in a right way

--- client/src/state/context/AuthContext.tsx
+++ client/src/state/context/AuthContext.tsx
@@ -1,63 +1,86 @@
-import { createContext, useContext, useEffect, useState } from "react"
-import { loginService, getMeService } from "../../services"
-import { Login, User } from "../../types"
+import { createContext, useContext, useState } from 'react';
+import { loginService, registerService } from '../../services';
+import { JwtResponse, NotificationData, User } from '../../types';
+import { LoginFormData, RegisterFormData } from '../../utils';
 
 interface AuthContextProps {
-	user: User | null
-	token: string | null
-	login: (data: Login) => Promise<void>
-	logout: () => void
+  user: User | null;
+  token: string | null;
+  login: (data: LoginFormData) => Promise<NotificationData>;
+  register: (data: RegisterFormData) => Promise<NotificationData>;
+  logout: () => void;
 }
 
-const AuthContext = createContext<AuthContextProps | undefined>(undefined)
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-	const [user, setUser] = useState<User | null>(null)
-	const [token, setToken] = useState<string | null>(localStorage.getItem("token"))
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem('token'),
+  );
 
-	useEffect(() => {
-		if (!token) return
-		;(async () => {
-			try {
-				if (token) {
-					const currentUser = await getMeService(token)
-					setUser(currentUser)
-				}
-			} catch {
-				logout()
-			}
-		})()
-	}, [])
+  const login = async (userData: LoginFormData): Promise<NotificationData> => {
+    try {
+      const data = await loginService(userData);
+      authenticate(data);
+      return { type: 'success', message: 'Login successful!' };
+    } catch (error) {
+      console.error('Login failed:', error);
+      return {
+        type: 'error',
+        message: 'Incorrect email/phone number or password.',
+      };
+    }
+  };
 
-	const login = async (userData: Login) => {
-		try {
-			const data = await loginService(userData)
+  const register = async (
+    userData: RegisterFormData,
+  ): Promise<NotificationData> => {
+    try {
+      await registerService(userData);
+      return {
+        type: 'success',
+        message:
+          'You have successfully register! Now, login in to your account.',
+      };
+    } catch (error) {
+      console.error('Register failed:', error);
+      return {
+        type: 'error',
+        message: 'Invalid credentials. Please try again.',
+      };
+    }
+  };
 
-			localStorage.setItem("token", data.Token)
-			localStorage.setItem("token_expiry", String(Date.now() + data.ExpiresInMinutes * 60 * 1000))
+  const logout = async () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('token_expiry');
+    localStorage.removeItem('user');
+    setUser(null);
+    setToken(null);
+  };
 
-			setToken(data.Token)
+  const authenticate = async (data: JwtResponse) => {
+    localStorage.setItem('token', data.token);
+    localStorage.setItem(
+      'token_expiry',
+      String(Date.now() + data.expiresIn * 60 * 1000),
+    );
+    localStorage.setItem('user', JSON.stringify(data.user));
 
-			const currentUser = await getMeService(data.Token)
-			setUser(currentUser)
-		} catch (error) {
-			console.error("Login failed:", error)
-			throw new Error("Invalid credentials. Please try again.")
-		}
-	}
+    setToken(data.token);
+    setUser(data.user);
+  };
 
-	const logout = () => {
-		localStorage.removeItem("token")
-		localStorage.removeItem("token_expiry")
-		setUser(null)
-		setToken(null)
-	}
-
-	return <AuthContext.Provider value={{ user, token, login, logout }}>{children}</AuthContext.Provider>
-}
+  return (
+    <AuthContext.Provider value={{ user, token, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const useAuth = () => {
-	const context = useContext(AuthContext)
-	if (!context) throw new Error("useAuth must be used within an AuthProvider")
-	return context
-}
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  return context;
+};
