aaf9d179b65b622fc60f28fe8e6e67004e75108b
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-13 18:39:57 +0200

    Removed old basket code fragments.

--- Library/DTOs/User/Basket.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Library.DTOs.User
-{
-    public class Basket
-    {
-        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
-        public decimal TotalPrice => Items.Sum(item => item.TotalPrice);
-    }
-}
--- UserAPI/Models/Basket.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserAPI.Models
-{
-    public class Basket
-    {
-        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
-        public decimal TotalPrice => Items.Sum(item => item.TotalPrice);
-    }
-}
