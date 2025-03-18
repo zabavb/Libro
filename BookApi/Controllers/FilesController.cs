98e0e6d20547aebf1dc8670afb2fcf8c73ec4ac0
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-03-18 03:14:47 +0200

    Integrate S3 storage and enhance Book API

---
+++ BookApi/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using Library.AWS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly S3StorageService _storageService;
+
+        public FilesController(S3StorageService storageService)
+        {
+            _storageService = storageService;
+        }
+
+        [HttpGet("signed-url")]
+        public IActionResult GetSignedUrl([FromQuery] string fileKey)
+        {
+            try
+            {
+                var signedUrl = _storageService.GenerateSignedUrl(fileKey);
+                return Ok(new { SignedUrl = signedUrl });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}
