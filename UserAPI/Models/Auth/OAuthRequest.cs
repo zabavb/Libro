87f59f59ad9922a48448c43126d2696c32d986fa
zabavb <bilonizkavik@gmail.com>
2025-03-20 10:23:27 +0100

    Created model OAuthRequest with Token data

---
+++ UserAPI/Models/Auth/OAuthRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAPI.Models.Auth
+{
+    public class OAuthRequest
+    {
+        public string Token { get; set; }
+
+        public OAuthRequest() => Token = string.Empty;
+    }
+}
