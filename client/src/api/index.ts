9ce4fbcbb8227803b8b7f80c7420a96b188113d7
zabavb <bilonizkavik@gmail.com>
2025-03-20 10:42:56 +0100

    Other changes due to: code foematting rules (frontend), fixing of library usages (backend)

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,44 +1,50 @@
-import { API_ROUTES } from "./config/apiConfig"
+import { API_ROUTES } from './config/apiConfig';
 
 // ================= AUTHENTICATION =================
 
-export const LOGIN = API_ROUTES.AUTH.LOGIN
-export const REGISTER = API_ROUTES.AUTH.REGISTER
+export const LOGIN = API_ROUTES.AUTH.LOGIN;
+export const REGISTER = API_ROUTES.AUTH.REGISTER;
 export const OAUTH = API_ROUTES.AUTH.OAUTH;
 
 // ================= USER =================
 
-export const USERS = API_ROUTES.USERS.BASE
+export const USERS = API_ROUTES.USERS.BASE;
 
-export const USERS_PAGINATED = (pageNumber: number = 1, pageSize: number = 10) =>
-	API_ROUTES.USERS.PAGINATED(pageNumber, pageSize)
+export const USERS_PAGINATED = (
+  pageNumber: number = 1,
+  pageSize: number = 10,
+) => API_ROUTES.USERS.PAGINATED(pageNumber, pageSize);
 
-export const USER_BY_ID = API_ROUTES.USERS.BY_ID
+export const USER_BY_ID = API_ROUTES.USERS.BY_ID;
 
 // ================= ORDER API  =================
-	//================= ORDER =================
-	export const ORDERS = API_ROUTES.ORDERS.BASE
+//================= ORDER =================
+export const ORDERS = API_ROUTES.ORDERS.BASE;
 
-	export const ORDERS_PAGINATED = (pageNumber: number = 1, pageSize: number = 10) =>
-		API_ROUTES.ORDERS.PAGINATED(pageNumber,pageSize)
+export const ORDERS_PAGINATED = (
+  pageNumber: number = 1,
+  pageSize: number = 10,
+) => API_ROUTES.ORDERS.PAGINATED(pageNumber, pageSize);
 
-	export const ORDER_BY_ID = API_ROUTES.ORDERS.BY_ID
+export const ORDER_BY_ID = API_ROUTES.ORDERS.BY_ID;
 
-	// ================= DELIVERY TYPE =================
+// ================= DELIVERY TYPE =================
 
-	export const DELIVERYTYPES = API_ROUTES.DELIVERY.BASE
+export const DELIVERYTYPES = API_ROUTES.DELIVERY.BASE;
 
-	export const DELIVERYTYPES_PAGINATED = (pageNumber: number = 1, pageSize: number = 10) =>
-		API_ROUTES.DELIVERY.PAGINATED(pageNumber,pageSize)
+export const DELIVERYTYPES_PAGINATED = (
+  pageNumber: number = 1,
+  pageSize: number = 10,
+) => API_ROUTES.DELIVERY.PAGINATED(pageNumber, pageSize);
 
-	export const DELIVERYTYPE_BY_ID = API_ROUTES.DELIVERY.BY_ID
+export const DELIVERYTYPE_BY_ID = API_ROUTES.DELIVERY.BY_ID;
 
+// ================= BOOK =================
+export const BOOKS = API_ROUTES.BOOKS.BASE;
 
-  // ================= BOOK =================
-  export const BOOKS = API_ROUTES.BOOKS.BASE
+export const BOOKS_PAGINATED = (
+  pageNumber: number = 1,
+  pageSize: number = 10,
+) => API_ROUTES.BOOKS.PAGINATED(pageNumber, pageSize);
 
-  export const BOOKS_PAGINATED = (pageNumber: number = 1, pageSize: number = 10) =>
-    API_ROUTES.BOOKS.PAGINATED(pageNumber, pageSize)
-
-  export const BOOK_BY_ID = API_ROUTES.BOOKS.BY_ID
-
+export const BOOK_BY_ID = API_ROUTES.BOOKS.BY_ID;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,8 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 58482,
-    }
-})
+  plugins: [plugin()],
+  server: {
+    port: 58482,
+  },
+});
