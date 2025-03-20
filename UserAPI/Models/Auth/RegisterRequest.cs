cf2f40036f64a5375b37f55940db2c99c2e019c7
zabavb <bilonizkavik@gmail.com>
2025-03-20 10:25:02 +0100

    Fixed incorrect validation rules like setting more optional user data

--- UserAPI/Models/Auth/RegisterRequest.cs
+++ UserAPI/Models/Auth/RegisterRequest.cs
@@ -10,15 +10,13 @@ namespace UserAPI.Models.Auth
         public string FirstName { get; set; }
 
         [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")] 
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address format.")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required.")]
         [Phone(ErrorMessage = "Invalid phone number format.")]
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
@@ -28,9 +26,6 @@ namespace UserAPI.Models.Auth
         public RegisterRequest()
         {
             FirstName = string.Empty;
-            LastName = string.Empty;
-            Email = string.Empty;
-            PhoneNumber = string.Empty;
             Password = string.Empty;
         }
     }
