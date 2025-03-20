2817992fa6f6e1570ebeef08128677f7cd2aa859
zabavb <bilonizkavik@gmail.com>
2025-03-20 10:40:48 +0100

    Updated registration with start up user's data in case of redirection a new user from oAuth authentication

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { RegisterFormData, registerSchema } from '../../utils';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { User } from '../../types';
 
 interface RegisterProps {
+  data: User | undefined;
   onSubmit: (userData: RegisterFormData) => Promise<void>;
 }
 
-const Register: React.FC<RegisterProps> = ({ onSubmit }) => {
+const Register: React.FC<RegisterProps> = ({ data, onSubmit }) => {
   const {
     register,
     handleSubmit,
@@ -20,13 +22,26 @@ const Register: React.FC<RegisterProps> = ({ onSubmit }) => {
     <div>
       <h2>Register</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('firstName')} placeholder='First Name' />
+        <input
+          {...register('firstName')}
+          placeholder='First Name'
+          value={data?.firstName}
+        />
         <p>{errors.firstName?.message}</p>
 
-        <input {...register('lastName')} placeholder='Last Name (Optional)' />
+        <input
+          {...register('lastName')}
+          placeholder='Last Name (Optional)'
+          value={data?.lastName}
+        />
         <p>{errors.lastName?.message}</p>
 
-        <input {...register('email')} placeholder='Email' type='email' />
+        <input
+          {...register('email')}
+          placeholder='Email'
+          type='email'
+          value={data?.email}
+        />
         <p>{errors.email?.message}</p>
 
         <input
