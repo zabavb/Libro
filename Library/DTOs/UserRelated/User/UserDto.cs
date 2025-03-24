925b36301d7bc252266dc6b084c089c2147f9487
zabavb <bilonizkavik@gmail.com>
2025-03-24 09:10:46 +0100

    Refcatoted UserDto and created new dto's UserCardDto, for displying user card with some related order's data, and UserDetailsDto, for dislaying all info about user, his order's with feedbacks and subscription

---
+++ Library/DTOs/UserRelated/User/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using Library.DTOs.User;
+
+namespace Library.DTOs.UserRelated.User
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public RoleType Role { get; set; }
+        public string ImageUrl { get; set; }
+
+        public UserDto()
+        {
+            Id = Guid.NewGuid();
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            DateOfBirth = DateTime.Today;
+            ImageUrl = string.Empty;
+            Role = RoleType.USER;
+        }
+    }
+}
