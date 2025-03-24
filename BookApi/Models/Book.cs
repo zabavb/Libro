905b0ede64e4a6c2f990787934e798bb28b2db1c
Yurii Salyha <yuriysalyga@gmail.com>
2025-03-24 21:15:27 +0100

    fixed book model

--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -5,27 +5,22 @@ namespace BookAPI.Models
 {
     public class Book
     {
-        public Guid Id { get; set; } 
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid DiscountId { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid PublisherId { get; set; }
         public string Title { get; set; } = null!;
-        public Guid AuthorId { get; set; } 
         public float Price { get; set; }
-        public Guid PublisherId { get; set; } 
         public Language Language { get; set; }
         public DateTime Year { get; set; }
-        public Guid CategoryId { get; set; } 
         public string Description { get; set; } = null!;
         public CoverType Cover { get; set; }
-<<<<<<< HEAD
-        public bool IsAvaliable { get; set; } = true; 
-        public Guid DiscountId { get; set; }
-
-=======
         public bool IsAvaliable { get; set; } = true;
         public string? ImageUrl { get; set; }
         public string? AudioFileUrl { get; set; }
         [NotMapped]
         public bool HasAudio => !string.IsNullOrEmpty(AudioFileUrl);
->>>>>>> f105b296e3830501faae87360aeaf508b350e1b6
         public Category Category { get; set; } = null!;
         public Publisher Publisher { get; set; } = null!;
         public Author Author { get; set; } = null!;
