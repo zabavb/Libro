231c3744f5138888bae097c7a97b2c10bb6f9c29
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-25 13:09:46 +0200

    Updated Interface to include second generic

--- OrderApi/Services/IOrderService.cs
+++ OrderApi/Services/IOrderService.cs
@@ -3,7 +3,7 @@ using Library.Interfaces;
 
 namespace OrderApi.Services
 {
-    public interface IOrderService : IManageable<OrderDto>
+    public interface IOrderService : IManageable<OrderDto,OrderDto>
     {
         Task<PaginatedResult<OrderDto>> GetAllAsync(int pageNumber, int pageSize, string searchTerm, Filter? filter, Sort? sort);
         Task DeleteAsync(Guid id);
