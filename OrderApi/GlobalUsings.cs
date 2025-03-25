fb39f4226116cf5934b375b2e70450cc748411b7
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-25 13:08:52 +0200

    Update UserDTO include

--- OrderApi/GlobalUsings.cs
+++ OrderApi/GlobalUsings.cs
@@ -1,6 +1,6 @@
 ﻿global using OrderDto = Library.DTOs.Order.Order;
 global using DeliveryTypeDto = Library.DTOs.Order.DeliveryType;
 global using Filter = OrderAPI.OrderFilter;
-global using UserDto = Library.DTOs.User.User;
+global using UserDto = Library.DTOs.UserRelated.User.UserDto;
 global using BookDto = Library.DTOs.Book.Book;
 global using Sort = OrderAPI.OrderSort;
