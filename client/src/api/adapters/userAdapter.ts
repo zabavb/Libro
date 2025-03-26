cd7f6db4c373e86eb27f0158a118aa98acaddf4e
zabavb <bilonizkavik@gmail.com>
2025-03-26 16:33:31 +0100

    Updated adapter for due to new marge-based data output

--- client/src/api/adapters/userAdapter.ts
+++ client/src/api/adapters/userAdapter.ts
@@ -1,37 +1,35 @@
-import { UserView, User, Role } from "../../types"
-import { dateToString } from "./commonAdapters"
+import { Role, User } from '../../types';
+import { UserFormData } from '../../utils';
 
 export const roleNumberToEnum = (roleNumber: number): Role => {
-	const roleMap: { [key: number]: Role } = {
-		0: Role.ADMIN,
-		1: Role.MODERATOR,
-		2: Role.USER,
-		3: Role.GUEST,
-	}
+  const roleMap: { [key: number]: Role } = {
+    0: Role.ADMIN,
+    1: Role.MODERATOR,
+    2: Role.USER,
+    3: Role.GUEST,
+  };
 
-	return roleMap[roleNumber] ?? Role.GUEST
-}
+  return roleMap[roleNumber] ?? Role.GUEST;
+};
 
 export const roleEnumToNumber = (role: Role): number => {
-	const roleMap: { [key in Role]: number } = {
-		[Role.ADMIN]: 0,
-		[Role.MODERATOR]: 1,
-		[Role.USER]: 2,
-		[Role.GUEST]: 3,
-	}
+  const roleMap: { [key in Role]: number } = {
+    [Role.ADMIN]: 0,
+    [Role.MODERATOR]: 1,
+    [Role.USER]: 2,
+    [Role.GUEST]: 3,
+  };
 
-	return roleMap[role] ?? 2
-}
+  return roleMap[role] ?? 2;
+};
 
-export const UserToUserView = (response: User): UserView => {
-	return {
-		id: response.id,
-		firstName: response.firstName,
-		lastName: response.lastName,
-		dateOfBirth: dateToString(response.dateOfBirth),
-		email: response.email,
-		phoneNumber: response.phoneNumber,
-		role: roleNumberToEnum(response.role),
-		imageUrl: response.imageUrl,
-	}
-}
+export const UserFormDataToUser = (form: UserFormData): User => ({
+  ...form,
+  id: '00000000-0000-0000-0000-000000000000',
+  lastName: form.lastName ?? null,
+  email: form.email ?? null,
+  phoneNumber: form.phoneNumber ?? null,
+  dateOfBirth: form.dateOfBirth ? new Date(form.dateOfBirth) : null,
+  role: roleEnumToNumber(form.role),
+  imageUrl: '',
+});
