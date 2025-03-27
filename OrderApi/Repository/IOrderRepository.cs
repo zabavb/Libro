0fd6582b21b0ec977955328813b9094d1c3fe2a3
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-27 22:09:35 +0200

    Created required merge functions

--- OrderApi/Repository/IOrderRepository.cs
+++ OrderApi/Repository/IOrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Common;
+using Library.DTOs.UserRelated.User;
 using Library.Interfaces;
 using OrderApi.Models;
 
@@ -6,4 +7,6 @@ public interface IOrderRepository : IManageable<Order, Order>
 {
     Task<PaginatedResult<Order>> GetAllPaginatedAsync(int pageNumber, int pageSize, string searchTerm, Filter? filter, Sort? sort);
     Task DeleteAsync(Guid id);
+    Task<IEnumerable<OrderDetailsSnippet>?> GetAllByUserId(Guid Id);
+    Task<OrderCardSnippet?> GetCardSnippetByUserId(Guid Id);
 }
