47593c08df73608eebe68240868f90556403a267
zabavb <bilonizkavik@gmail.com>
2025-03-28 10:58:30 +0100

    Implemented ExceptionMiddleware with some other custom exceptions for readable and consistant middleware's work

---
+++ Library/Common/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace Library.Common.Middleware
+{
+    public class ExceptionMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = ex switch
+            {
+                KeyNotFoundException => (int)HttpStatusCode.NotFound,
+                UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+                ArgumentException or InvalidOperationException => (int)HttpStatusCode.BadRequest,
+                RepositoryException => (int)HttpStatusCode.InternalServerError,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+
+            var errorResponse = new ErrorResponse
+            {
+                StatusCode = statusCode,
+                Message = ex.Message,
+                Details = ex.InnerException?.Message
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+        }
+    }
+}
---
+++ Library/Common/Middleware/RepositoryException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Common.Middleware
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException(string message) : base(message) { }
+        public RepositoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
