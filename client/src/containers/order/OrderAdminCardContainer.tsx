62ecf43e873dc86457fd9de86dd0b790ec0fea44
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-30 16:36:08 +0300

    Updated containers to remove redux

--- client/src/containers/order/OrderAdminCardContainer.tsx
+++ client/src/containers/order/OrderAdminCardContainer.tsx
@@ -1,13 +1,11 @@
 import { useDispatch } from "react-redux";
 import { Order } from "../../types";
-import { AppDispatch, removeOrder } from "../../state/redux";
-import { useSelector } from "react-redux";
-import { RootState } from "../../state/redux/store";
+import { AppDispatch } from "../../state/redux";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { resetOrderOperationStatus } from "../../state/redux/slices/orderSlice";
 import OrderAdminCard from "../../components/order/admin/OrderAdminCard";
 import { OrderToOrderView } from "../../api/adapters/orderAdapters";
+import { removeOrderService } from "../../services";
+import { addNotification } from "../../state/redux/slices/notificationSlice";
 
 interface OrderAdminCardContainerProps {
     order: Order
@@ -15,28 +13,30 @@ interface OrderAdminCardContainerProps {
 
 const OrderAdminCardContainter: React.FC<OrderAdminCardContainerProps> = ({ order }) => {
     const dispatch = useDispatch<AppDispatch>()
-    const { operationStatus, error} = useSelector((state: RootState) => state.orders)
-    const orderView = OrderToOrderView(order)
     const navigate = useNavigate()
 
-    const handleDelete = (e: React.MouseEvent) => {
+    const orderView = OrderToOrderView(order)
+
+
+    const handleDelete = async (e: React.MouseEvent) => {
         e.stopPropagation()
-        dispatch(removeOrder(order.id))
+        const response = await removeOrderService(order.id)
+        dispatch(
+              response.error
+                ? addNotification({
+                    message: response.error,
+                    type: 'error',
+                  })
+                : addNotification({
+                    message: 'Order successfully deleted.',
+                    type: 'success',
+                  }),
+            );
     }
 
     const handleNavigate = () => {
         navigate(`/admin/orders/${order.id}`)
     }
-
-    useEffect(() => {
-        if (operationStatus === "success") {
-            alert("Order removed successfully!")
-            dispatch(resetOrderOperationStatus())
-        }else if (operationStatus === "error"){
-            alert(error)
-            dispatch(resetOrderOperationStatus())
-        }
-    }, [operationStatus, error, dispatch])
 
     return (
         <OrderAdminCard
