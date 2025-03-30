1e9502a568fb8c4781973f0a344057c824c087ce
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-03-30 23:48:14 +0300

    Removed delivery type slice from redux

--- client/src/state/redux/index.ts
+++ client/src/state/redux/index.ts
@@ -1,5 +1,3 @@
 export { default as store } from "./store";
 export type { RootState, AppDispatch } from "./store";
-export { fetchOrders, addOrder, editOrder, removeOrder } from "./slices/orderSlice"
-export { fetchDeliveryTypes, addDeliveryType, editDeliveryType, removeDeliveryType} from "./slices/deliveryTypeSlice"
 export { fetchBooks } from "./slices/bookSlice"
