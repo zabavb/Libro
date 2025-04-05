e3062bb7f3d1676b06cf4fb234d3f40eec43ecb8
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-04-05 17:08:25 +0300

    Removed 'add sample' button from Cart

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux"
 import store from "./state/redux/store"
 import AppRoutes from "./routes.tsx"
 import NotificationContainer from "./containers/common/NotificationContainer.tsx"
-
+import './index.css'
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider store={store}>
