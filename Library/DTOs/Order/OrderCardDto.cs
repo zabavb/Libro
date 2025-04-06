8d53693a7e89080f57746d61b03198d1c99c4728
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-04-07 01:15:43 +0300

    Created Card & Snippet classes

---
+++ Library/DTOs/Order/OrderCardDto.cs
@@ -0,0 +1,39 @@
+﻿using Library.Interfaces;
+
+namespace Library.DTOs.Order
+{
+    public class OrderCardDto
+    {
+        public Guid OrderId { get; set; }
+        public string Region { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public float FullPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public CollectionSnippet<BookCardSnippet> Books { get; set; }
+        public SingleSnippet<DeliveryCardSnippet> Delivery { get; set; }
+
+        public OrderCardDto()
+        {
+            Region = string.Empty;
+            Address = string.Empty;
+            City = string.Empty;
+        }
+    }
+
+    public class BookCardSnippet
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public BookCardSnippet() => Title = string.Empty;
+    }
+
+    public class DeliveryCardSnippet
+    {
+        public Guid DeliveryId { get; set; }
+        public string ServiceName { get; set; }
+        public DeliveryCardSnippet() => ServiceName = string.Empty;
+    }
+}
