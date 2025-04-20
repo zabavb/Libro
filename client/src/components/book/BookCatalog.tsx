43c83962f0119ed7834ea972dd7758eb16ac231f
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-04-20 21:37:41 +0300

    Barebone catalog styling

--- client/src/components/book/BookCatalog.tsx
+++ client/src/components/book/BookCatalog.tsx
@@ -2,7 +2,7 @@ import { Book, BookSort } from "@/types";
 import { BookFilter } from "@/types/filters/BookFilter";
 import Pagination from "../common/Pagination";
 import BookCardContainer from "@/containers/books/BookCardContainer";
-
+import "@/assets/styles/components/book/catalog.css"
 interface BookCatalogProps {
     books?: Book[]
     loading: boolean
@@ -31,29 +31,54 @@ const BookCatalog: React.FC<BookCatalogProps> = ({
 }) => {
     if (loading) return <p>Loading...</p>
     return(
-        <div>
-            {/* Search panel */}
+        <div className="catalog-container">
+            <aside className="options-panel">
+                {/* Temporary text, to be replaced 
+                with actual components in the future */}
+                <div className="options-container">
+                    <div>Filters</div>
+                    <div>Categories</div>
+                    <div>Types</div>
+                    <div>Availability</div>
+                    <div>Language</div>
+                    <div>Author</div>
+                </div>
+                <div>
+                    <div>Price</div>
+                </div>
+            </aside>
+            <main className="books-panel">
+                <div className="books-panel-header">
+                    <div className="books-counter">
+                        {pagination.totalCount} books
+                    </div>
+                    <div className="opacity-50 font-semibold text-base">
+                        SORT_COMPONENT_TMP
+                    </div>
+                </div>
+                <div className="books-panel-main">
+                {loading ? (<>tmp</>) : books.length > 0 ? (
+                    books.map((book) => (
+                        <BookCardContainer
+                            key={book.bookId}
+                            book={book}
+                            />
+                    ))
+                ) : (
+                    <p>No books found.</p>
+                )}
+                </div>
+                <div className="books-panel-footer">
+                    {/* Replace with custom pagination */}
+                    <button className="load-btn">Load more books</button>
+                    <Pagination
+                        pagination={pagination}
+                        onPageChange={onPageChange}
+                    />
+                </div>
+            </main>
 
-            {/* Filter panel */}
 
-            {/* Sort panel */}
-            <div style={{display:'flex'}}>
-            {loading ? (<>tmp</>) : books.length > 0 ? (
-                books.map((book) => (
-                    <BookCardContainer
-                        key={book.bookId}
-                        book={book}
-                        />
-                ))
-            ) : (
-                <p>No books found.</p>
-            )}
-            </div>
-
-            <Pagination
-                pagination={pagination}
-                onPageChange={onPageChange}
-            />
             
         </div>
     )
