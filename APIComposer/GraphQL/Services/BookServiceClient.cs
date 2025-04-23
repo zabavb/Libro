f68bfc543146f5f1e02e73da14871dd71a5a383f
zabavb <bilonizkavik@gmail.com>
2025-04-23 15:12:52 +0200

    Added transportation of authentication JWT tokens trough GraphQL layer

--- APIComposer/GraphQL/Services/BookServiceClient.cs
+++ APIComposer/GraphQL/Services/BookServiceClient.cs
@@ -1,16 +1,29 @@
+using System.Net.Http.Headers;
 using APIComposer.GraphQL.Services.Interfaces;
 using Library.DTOs.UserRelated.User;
 
 namespace APIComposer.GraphQL.Services
 {
-    public class BookServiceClient(HttpClient http) : IBookServiceClient
+    public class BookServiceClient(HttpClient http, IHttpContextAccessor httpContextAccessor) : IBookServiceClient
     {
         private readonly HttpClient _http = http;
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+        private void SetAuthHeader()
+        {
+            var token = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].FirstOrDefault();
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                _http.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token.Replace("Bearer ", ""));
+            }
+        }
 
         public async Task<ICollection<string>> GetAllBookNamesAsync(ICollection<Guid> ids)
         {
             try
             {
+                SetAuthHeader();
                 var queryString = string.Join("&ids=", ids);
                 var response =
                     await _http.GetFromJsonAsync<ICollection<string>>($"books/for-user/details?ids={queryString}");
@@ -26,9 +39,10 @@ namespace APIComposer.GraphQL.Services
         {
             try
             {
+                SetAuthHeader();
                 var response =
                     await _http.GetFromJsonAsync<ICollection<FeedbackForUserDetails>>(
-                        "feedbacks/for-user/details/{id}");
+                        $"feedbacks/for-user/details/{userId}");
                 return response ?? new List<FeedbackForUserDetails>();
             }
             catch (Exception)
