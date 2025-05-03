7adb5adaeca418ab3b5d6156f7ea55c64a291fb3
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-03 19:54:47 +0300

    Created an icon library. small bugfix

---
+++ client/src/lib/icons.ts
@@ -0,0 +1,40 @@
+import bUser from '@/assets/icons/headerUser.svg'
+import wBonus from '@/assets/icons/userBonus.svg'
+import wTruck from '@/assets/icons/userTruck.svg'
+import bHeart from '@/assets/icons/headerHeart.svg'
+import wBookmark from '@/assets/icons/userBookmark.svg'
+import wGear from '@/assets/icons/userSettings.svg'
+import wEnter from '@/assets/icons/userLogout.svg'
+import wBook from '@/assets/icons/adminBook.svg'
+import bCart from '@/assets/icons/cartIcon.svg'
+import wCart from '@/assets/icons/cartBig.svg'
+import wFile from '@/assets/icons/adminOrder.svg'
+import wDiagram from '@/assets/icons/adminMain.svg'
+import wCredit from '@/assets/icons/adminSubscription.svg'
+import wUser from '@/assets/icons/adminUser.svg'
+import wCaretDown from '@/assets/icons/caretDown.svg'
+import wHamburger from '@/assets/icons/hamburgerIcon.svg'
+import wLogout from '@/assets/icons/logoutIcon.svg'
+import bMagnifyingGlass from '@/assets/icons/magnifyingGlass.svg'
+import gMenuClose from '@/assets/icons/menuClose.svg'
+export const icons = {
+    wUser,
+    wBonus,
+    wTruck,
+    bHeart,
+    wBookmark,
+    wEnter,
+    wGear,
+    wBook,
+    bCart,
+    wCart,
+    wFile,
+    wDiagram,
+    wCredit,
+    bUser,
+    wCaretDown,
+    wHamburger,
+    wLogout,
+    bMagnifyingGlass,
+    gMenuClose
+}
