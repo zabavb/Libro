325de416f107e5339e4e5e4dd99647334d39d2f2
Mariia Kandyba <mariia.kandyba@gmail.com>
2025-05-11 20:10:56 +0300

    Files added to seeder

--- BookApi/Data/DataSeeder.cs
+++ BookApi/Data/DataSeeder.cs
@@ -23,22 +23,79 @@ namespace BookAPI.Data
             {
                 Guid.NewGuid(),
                 Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
                 Guid.NewGuid()
             };
-            var localAudioPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Files", "MBp1.mp3");
-            var pdflocalAudioPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Files", "Ticks.pdf");
-
-
             var imagePaths = new Dictionary<string, string>
-            {
-                { "Місто зі скла", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSVPcBg9gdzf2mit382PYIbFkkDbn-JB7jA&s" },
-                { "Тіні минулого", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSVPcBg9gdzf2mit382PYIbFkkDbn-JB7jA&s" },
-                { "Емоційний інтелект", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSVPcBg9gdzf2mit382PYIbFkkDbn-JB7jA&s" }
-            };
-            var audios = new List<string>
-            {
-                "https://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg" ,
-            };
+{
+    { "Місто зі скла", "Data/Files/Images/місто_зі_скла.jpeg" },
+    { "Тіні минулого", "Data/Files/Images/тіні_минулого.jpeg" },
+    { "Емоційний інтелект", "Data/Files/Images/емоційний_інтелект.jpeg" },
+    { "У пошуках світла", "Data/Files/Images/у_пошуках_світла.jpeg" },
+    { "Поза межами розуму", "Data/Files/Images/поза_межами_розуму.jpeg" },
+    { "Код надії", "Data/Files/Images/код_надії.jpeg" },
+    { "Таємниця лісу", "Data/Files/Images/таємниця_лісу.jpeg" },
+    { "Історії з майбутнього", "Data/Files/Images/історії_з_майбутнього.jpeg" },
+    { "Психологія впливу", "Data/Files/Images/психологія_впливу.jpeg" },
+    { "Книга мандрівника", "Data/Files/Images/книга_мандрівника.jpeg" },
+    { "Фізика для допитливих", "Data/Files/Images/фізика_для_допитливих.jpeg" },
+    { "Біологія життя", "Data/Files/Images/біологія_життя.jpeg" },
+    { "Магія свідомості", "Data/Files/Images/магія_свідомості.jpeg" },
+    { "Кохання у віртуальному світі", "Data/Files/Images/кохання_у_віртуальному_світі.jpeg" },
+    { "Всесвіт всередині нас", "Data/Files/Images/всесвіт_всередині_нас.jpeg" }
+};
+
+
+            var pdfPaths = new Dictionary<string, string>
+{
+    { "Місто зі скла", "Data/Files/Pdfs/місто_зі_скла.pdf" },
+    { "Тіні минулого", "Data/Files/Pdfs/тіні_минулого.pdf" },
+    { "Емоційний інтелект", "Data/Files/Pdfs/емоційний_інтелект.pdf" },
+    { "У пошуках світла", "Data/Files/Pdfs/у_пошуках_світла.pdf" },
+    { "Поза межами розуму", "Data/Files/Pdfs/поза_межами_розуму.pdf" },
+    { "Код надії", "Data/Files/Pdfs/код_надії.pdf" },
+    { "Таємниця лісу", "Data/Files/Pdfs/таємниця_лісу.pdf" },
+    { "Історії з майбутнього", "Data/Files/Pdfs/історії_з_майбутнього.pdf" },
+    { "Психологія впливу", "Data/Files/Pdfs/психологія_впливу.pdf" },
+    { "Книга мандрівника", "Data/Files/Pdfs/книга_мандрівника.pdf" },
+    { "Фізика для допитливих", "Data/Files/Pdfs/фізика_для_допитливих.pdf" },
+    { "Біологія життя", "Data/Files/Pdfs/біологія_життя.pdf" },
+    { "Магія свідомості", "Data/Files/Pdfs/магія_свідомості.pdf" },
+    { "Кохання у віртуальному світі", "Data/Files/Pdfs/кохання_у_віртуальному_світі.pdf" },
+    { "Всесвіт всередині нас", "Data/Files/Pdfs/всесвіт_всередині_нас.pdf" }
+};
+
+            var audioPaths = new Dictionary<string, string>
+{
+    { "Місто зі скла", "Data/Files/Audios/місто_зі_скла.mp3" },
+    { "Тіні минулого", "Data/Files/Audios/тіні_минулого.mp3" },
+    { "Емоційний інтелект", "Data/Files/Audios/емоційний_інтелект.mp3" },
+    { "У пошуках світла", "Data/Files/Audios/у_пошуках_світла.mp3" },
+    { "Поза межами розуму", "Data/Files/Audios/поза_межами_розуму.mp3" },
+    { "Код надії", "Data/Files/Audios/код_надії.mp3" },
+    { "Таємниця лісу", "Data/Files/Audios/таємниця_лісу.mp3" },
+    { "Історії з майбутнього", "Data/Files/Audios/історії_з_майбутнього.mp3" },
+    { "Психологія впливу", "Data/Files/Audios/психологія_впливу.mp3" },
+    { "Книга мандрівника", "Data/Files/Audios/книга_мандрівника.mp3" },
+    { "Фізика для допитливих", "Data/Files/Audios/фізика_для_допитливих.mp3" },
+    { "Біологія життя", "Data/Files/Audios/біологія_життя.mp3" },
+    { "Магія свідомості", "Data/Files/Audios/магія_свідомості.mp3" },
+    { "Кохання у віртуальному світі", "Data/Files/Audios/кохання_у_віртуальному_світі.mp3" },
+    { "Всесвіт всередині нас", "Data/Files/Audios/всесвіт_всередині_нас.mp3" }
+};
+
+
 
             var categories = new List<Category>
             {
@@ -55,7 +112,6 @@ namespace BookAPI.Data
             };
             modelBuilder.Entity<Category>().HasData(categories);
 
-            // Додавання підкатегорій
             var subCategories = new List<SubCategory>
             {
                 // Підкатегорії для "Фантастика"
@@ -167,9 +223,6 @@ namespace BookAPI.Data
             };
 
             modelBuilder.Entity<Author>().HasData(authors);
-
-
-
             var random = new Random();
             var discounts = bookIds.Select(bookId => new Discount
             {
@@ -179,10 +232,7 @@ namespace BookAPI.Data
                 StartDate = DateTime.UtcNow,
                 EndDate = DateTime.UtcNow.AddMonths(1)
             }).ToList();
-
             modelBuilder.Entity<Discount>().HasData(discounts);
-
-
             var books = new List<Book>
             {
                 new Book
@@ -198,10 +248,7 @@ namespace BookAPI.Data
                     Description = "Фантастичний роман про місто, побудоване зі скла.",
                     Cover = CoverType.HARDCOVER,
                     Quantity = 5,
-                    AudioFileUrl = await filesHelper.UploadAudioAsync(localAudioPath, bookIds[0]),
-                    ImageUrl = await filesHelper.UploadImageAsync(imagePaths["Місто зі скла"], bookIds[0]),
                     DiscountId = discounts[0].DiscountId,
-                    PdfFileUrl = await filesHelper.UploadPdfAsync(pdflocalAudioPath, bookIds[0]),
                 },
                 new Book
                 {
@@ -217,9 +264,6 @@ namespace BookAPI.Data
                     Cover = CoverType.SOFTCOVER,
                     Quantity = 3,
                     DiscountId = discounts[1].DiscountId,
-                    AudioFileUrl = await filesHelper.UploadAudioAsync(localAudioPath, bookIds[1]),
-                    ImageUrl = await filesHelper.UploadImageAsync(imagePaths["Тіні минулого"], bookIds[1]),
-                    PdfFileUrl = await filesHelper.UploadPdfAsync(pdflocalAudioPath, bookIds[1]),
                 },
                 new Book
                 {
@@ -235,17 +279,216 @@ namespace BookAPI.Data
                     Cover = CoverType.HARDCOVER,
                     Quantity = 1,
                     DiscountId = discounts[2].DiscountId,
-                    AudioFileUrl = await filesHelper.UploadAudioAsync(localAudioPath, bookIds[2]),
-                    ImageUrl = await filesHelper.UploadImageAsync(imagePaths["Емоційний інтелект"], bookIds[2]),
-                    PdfFileUrl = await filesHelper.UploadPdfAsync(pdflocalAudioPath, bookIds[2]),
-                }
+                },
+                new Book
+                 {
+                     Id = bookIds[3],
+                     Title = "У пошуках світла",
+                     AuthorId = authors[3].Id,
+                     PublisherId = publishers[0].Id,
+                     CategoryId = categories[3].Id,
+                     Price = 299.99f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2020, 1, 1),
+                     Description = "Натхненна історія про подолання труднощів.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 2,
+                     DiscountId = discounts[3].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[4],
+                     Title = "Поза межами розуму",
+                     AuthorId = authors[4].Id,
+                     PublisherId = publishers[3].Id,
+                     CategoryId = categories[4].Id,
+                     Price = 370.00f,
+                     Language = Language.ENGLISH,
+                     Year = new DateTime(2021, 5, 10),
+                     Description = "Науково-популярна книга про можливості людського мозку.",
+                     Cover = CoverType.HARDCOVER,
+                     Quantity = 4,
+                     DiscountId = discounts[4].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[5],
+                     Title = "Код надії",
+                     AuthorId = authors[2].Id,
+                     PublisherId = publishers[2].Id,
+                     CategoryId = categories[0].Id,
+                     Price = 310.00f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2022, 11, 15),
+                     Description = "Фантастична пригода у постапокаліптичному світі.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 7,
+                     DiscountId = discounts[0].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[6],
+                     Title = "Таємниця лісу",
+                     AuthorId = authors[1].Id,
+                     PublisherId = publishers[1].Id,
+                     CategoryId = categories[2].Id,
+                     Price = 260.00f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2019, 3, 7),
+                     Description = "Детектив у стилі нуар серед дикої природи.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 3,
+                     DiscountId = discounts[1].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[7],
+                     Title = "Історії з майбутнього",
+                     AuthorId = authors[0].Id,
+                     PublisherId = publishers[0].Id,
+                     CategoryId = categories[0].Id,
+                     Price = 390.00f,
+                     Language = Language.ENGLISH,
+                     Year = new DateTime(2024, 2, 20),
+                     Description = "Збірка оповідань про альтернативні реальності.",
+                     Cover = CoverType.HARDCOVER,
+                     Quantity = 6,
+                     DiscountId = discounts[3].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[8],
+                     Title = "Психологія впливу",
+                     AuthorId = authors[2].Id,
+                     PublisherId = publishers[4].Id,
+                     CategoryId = categories[4].Id,
+                     Price = 330.00f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2020, 6, 1),
+                     Description = "Практичне керівництво по впливу на людей.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 5,
+                     DiscountId = discounts[2].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[9],
+                     Title = "Книга мандрівника",
+                     AuthorId = authors[3].Id,
+                     PublisherId = publishers[2].Id,
+                     CategoryId = categories[3].Id,
+                     Price = 280.00f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2018, 7, 15),
+                     Description = "Путівник та мотиваційна література для мандрівників.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 2,
+                     DiscountId = discounts[4].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[10],
+                     Title = "Фізика для допитливих",
+                     AuthorId = authors[4].Id,
+                     PublisherId = publishers[3].Id,
+                     CategoryId = categories[5].Id,
+                     Price = 310.50f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2021, 9, 5),
+                     Description = "Легкий вступ до складних фізичних понять.",
+                     Cover = CoverType.HARDCOVER,
+                     Quantity = 4,
+                     DiscountId = discounts[1].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[11],
+                     Title = "Біологія життя",
+                     AuthorId = authors[4].Id,
+                     PublisherId = publishers[3].Id,
+                     CategoryId = categories[5].Id,
+                     Price = 299.99f,
+                     Language = Language.ENGLISH,
+                     Year = new DateTime(2023, 4, 12),
+                     Description = "Ілюстроване видання для вивчення біології.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 6,
+                     DiscountId = discounts[3].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[12],
+                     Title = "Магія свідомості",
+                     AuthorId = authors[2].Id,
+                     PublisherId = publishers[4].Id,
+                     CategoryId = categories[4].Id,
+                     Price = 355.75f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2020, 10, 30),
+                     Description = "Як мозок формує нашу реальність.",
+                     Cover = CoverType.HARDCOVER,
+                     Quantity = 1,
+                     DiscountId = discounts[2].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[13],
+                     Title = "Кохання у віртуальному світі",
+                     AuthorId = authors[1].Id,
+                     PublisherId = publishers[1].Id,
+                     CategoryId = categories[1].Id,
+                     Price = 275.99f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2023, 8, 8),
+                     Description = "Сучасна історія кохання у цифрову епоху.",
+                     Cover = CoverType.SOFTCOVER,
+                     Quantity = 3,
+                     DiscountId = discounts[1].DiscountId,
+                 },
+                 new Book
+                 {
+                     Id = bookIds[14],
+                     Title = "Всесвіт всередині нас",
+                     AuthorId = authors[0].Id,
+                     PublisherId = publishers[2].Id,
+                     CategoryId = categories[5].Id,
+                     Price = 380.00f,
+                     Language = Language.UKRAINIAN,
+                     Year = new DateTime(2019, 12, 25),
+                     Description = "Наукові відкриття про людське тіло та свідомість.",
+                     Cover = CoverType.HARDCOVER,
+                     Quantity = 5,
+                     DiscountId = discounts[4].DiscountId,
+                 }
             };
             modelBuilder.Entity<Book>().HasData(books);
+
+            foreach (var book in books)
+            {
+                var imageFilePath = imagePaths[book.Title];
+                var pdfFilePath = pdfPaths[book.Title];
+                var audioFilePath = audioPaths[book.Title];
+
+                var imageFile = filesHelper.GetFormFileFromPath(imageFilePath, "image/jpeg");
+                var pdfFile = filesHelper.GetFormFileFromPath(pdfFilePath, "application/pdf");
+                var audioFile = filesHelper.GetFormFileFromPath(audioFilePath, "audio/mpeg");
+
+                var imageUrl = await filesHelper.UploadImageFromFormAsync(imageFile, book.Id);
+                var pdfUrl = await filesHelper.UploadPdfFromFormAsync(pdfFile, book.Id);
+                var audioUrl = await filesHelper.UploadAudioFromFormAsync(audioFile, book.Id);
+
+                book.ImageUrl = imageUrl;
+                book.PdfFileUrl = pdfUrl;
+                book.AudioFileUrl = audioUrl;
+            }
 
 
             modelBuilder.Entity("BookSubCategory").HasData(
                 new { BookId = books[0].Id, SubCategoryId = subCategories[0].Id },
+                new { BookId = books[0].Id, SubCategoryId = subCategories[1].Id },
+                new { BookId = books[0].Id, SubCategoryId = subCategories[2].Id },
                 new { BookId = books[1].Id, SubCategoryId = subCategories[3].Id },
+                new { BookId = books[1].Id, SubCategoryId = subCategories[4].Id },
                 new { BookId = books[2].Id, SubCategoryId = subCategories[12].Id }
             );
 
