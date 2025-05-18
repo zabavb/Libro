4ba267158b7cf10e12bb9c9fc6c76536e3414a1c
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-18 21:44:56 +0300

    Updated and stylized book admin list

--- client/src/components/book/admin/BookAdminCard.tsx
+++ client/src/components/book/admin/BookAdminCard.tsx
@@ -1,60 +1,43 @@
-import React from "react";
-import { BookView } from "../../../types";
-
+import { Book } from "../../../types";
+import { icons } from '@/lib/icons'
 interface BookAdminCardProps {
-    book: BookView;
-    onDelete: (e: React.MouseEvent) => void;
-    onNavigate: () => void;
+    book: Book
+    onDelete: (e: React.MouseEvent) => void
+    onNavigate: () => void 
 }
 
-const BookAdminCard: React.FC<BookAdminCardProps> = ({ book, onDelete, onNavigate }) => {
-    return (
+const BookAdminCard: React.FC<BookAdminCardProps> = ({book, onDelete,onNavigate}) => {
+    const bookUid = book.bookId.split('-')[4];
+    return(
         <>
-            <hr />
-            <li
-                onClick={(e) => {
-                    e.stopPropagation();
-                    onNavigate();
-                }}>
-                <div>
-                    <p>
-                        <strong>Title:</strong> {book.title}
-                    </p>
-                    <p>
-                        <strong>Year:</strong> {book.year}
-                    </p>
-                    <p>
-                        <strong>Cover:</strong> {book.cover}
-                    </p>
-                    <p>
-                        <strong>Price:</strong> {book.price}
-                    </p>
-                    <p>
-                        <strong>Discount:</strong> {book.discountRate} <strong> %</strong>
-                    </p>
-                    <p>
-                        <strong>End of discount:</strong> {book.endDate.toString()}
-                    </p>
-                    <p>
-                        <strong>Language:</strong> {book.language}
-                    </p>
-                    {/*<p>*/}
-                    {/*    <strong>Available:</strong> {book.isAvailable ? "Yes" : "No"}*/}
-                    {/*</p>*/}
-                    <p>
-                        <strong>Quantity:</strong> {book.quantity}
-                    </p>
-                    <button
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            onDelete(e);
-                        }}>
-                        Delete
-                    </button>
-                </div>
-            </li>
+            <tr>
+                <td className="flex justify-center">
+                    <img className="w-[50px] h-[70px]" src={`https://picsum.photos/seed/${book.bookId}/50/70`} />
+                </td>
+                <td>
+                    {book.title}
+                </td>
+                <td>
+                    AUTHOR TBI
+                </td>
+                <td>
+                    {bookUid}
+                </td>
+                <td>
+                    GENRE TBI
+                </td>
+                <td>
+                    {book.price}
+                </td>
+                <td>
+                    <div className='flex gap-2'>
+                        <button onClick={onNavigate} className='p-2.5 bg-[#FF642E] rounded-lg'><img src={icons.wPen} /></button>
+                        <button onClick={onDelete} className='p-2.5 border-[1px] border-[#FF642E] rounded-lg'><img src={icons.oTrash} /></button>
+                    </div>
+                </td>
+            </tr>
         </>
-    );
-};
+    )
+}
 
-export default BookAdminCard;
+export default BookAdminCard
--- client/src/containers/books/BookAdminCardContainer.tsx
+++ client/src/containers/books/BookAdminCardContainer.tsx
@@ -1,32 +1,49 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
 import { Book } from "../../types/objects/Book"
-import { BookToBookView } from "../../api/adapters/bookAdapter"
+// import { BookToBookView } from "../../api/adapters/bookAdapter"
 import BookAdminCard from "@/components/book/admin/BookAdminCard"
+import { deleteBookService } from "@/services/bookService"
+import { useDispatch } from "react-redux"
+import { AppDispatch } from "@/state/redux"
+import { addNotification } from "@/state/redux/slices/notificationSlice"
 
 interface BookCardContainerProps {
-	book: Book
+  book: Book
 }
 
 const BookAdminCardContainer: React.FC<BookCardContainerProps> = ({ book }) => {
-	console.log("Book in BookCardContainer:", book);
-	const bookView = BookToBookView(book)
-	const navigate = useNavigate()
-	const handleNavigate = () => {
-		navigate(`/admin/books/${book.bookId}`)
-	}
+    const dispatch = useDispatch<AppDispatch>()
+  // const bookView = BookToBookView(book) // TEMPORARY REMOVED
+  const navigate = useNavigate()
+  const handleNavigate = () => {
+    navigate(`/admin/booksRelated/book/${book.bookId}`)
+  }
 
-	const handleDelete = () => {
-		console.log("DELETE")
-	}
+  const handleDelete = async (e: React.MouseEvent) => {
+    e.stopPropagation()
+    const response = await deleteBookService(book.bookId)
+    dispatch(
+        response.error
+        ? addNotification({
+          message: response.error,
+          type: 'error',
+          })
+        : addNotification({
+          message: 'Book successfully deleted.',
+          type: 'success',
+          }),
+      );
+  }
 
-	return (
-		<BookAdminCard
-			book={bookView}
-			onNavigate={handleNavigate}
-			onDelete={handleDelete}
-		/>
-	)
+
+  return (
+    <BookAdminCard
+      book={book}
+      onNavigate={handleNavigate}
+      onDelete={handleDelete}
+    />
+  )
 }
 
 export default BookAdminCardContainer
