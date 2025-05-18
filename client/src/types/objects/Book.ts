7110d576465f2be822875d40245583b5b4f0ef36
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-18 21:35:25 +0300

    Updated book interface to match backend's DTO

--- client/src/types/objects/Book.ts
+++ client/src/types/objects/Book.ts
@@ -1,21 +1,20 @@
 
 export interface Book {
-	bookId: string
-	title: string
-	authorId: string
-	publisherId: string
-	price: number
-	language: number
-	year: Date
-	description?: string
-	cover: number
-    quantity: number
-	//isAvailable: boolean
-	feedbackIds: string[]
-	discountRate: number;
-	startDate: Date;
-	endDate: Date;
-	audioFileUrl: string;
-	//subCategoryIds: string[]
-	imageUrl?: string;
+  bookId: string;
+  title: string;
+  authorId: string;
+  publisherId: string;
+  categoryId: string;
+  discountId?: string;
+  price: number
+  language: number
+  year: Date
+  description?: string
+  cover: number
+  quantity: number
+  imageUrl?: string;
+  audioFileUrl?: string;
+  PdfFileUrl?: string;
+  feedbackIds: string[];
+  subcategoryIds: string[]; 
 }
