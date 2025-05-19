1389909c273af8885310b983c7984a724e4ea029
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-19 15:20:30 +0300

    Fixed cart styles

--- client/src/components/common/CartCard.tsx
+++ client/src/components/common/CartCard.tsx
@@ -39,7 +39,7 @@ const CartCard: React.FC<CartCardProps> = ({ item, onAdd, onRemove, onItemClear 
 
     return (
         
-        <div className="flex gap-[14px] text-dark relative">
+        <div className="flex gap-[14px] text-[#1A1D23] relative">
             <p className="delete-btn"
              onClick={() => onItemClear(item.bookId)}>
                 Delete
@@ -47,12 +47,12 @@ const CartCard: React.FC<CartCardProps> = ({ item, onAdd, onRemove, onItemClear 
             <img src={`https://picsum.photos/seed/${item.bookId}/80/120`} />
             <div className="flex flex-col gap-4">
                 <div>
-                    <p className="text-black">{book?.title}</p>
-                    <p className="text-gray">AUTHOR_TMP</p>
+                    <p className="text-[#1A1D23]">{book?.title}</p>
+                    <p className="text-[#929089]">AUTHOR_TMP</p>
                 </div>
                 <div>
                     <p>{book?.price} UAH</p>
-                    <p className="text-accent">{(book?.quantity ?? 0 > 0) ? "Available" : "Not available"}</p>
+                    <p className="text-[#FF642E]">{(book?.quantity ?? 0 > 0) ? "Available" : "Not available"}</p>
                 </div>
                 <div className="flex gap-2.5 items-center">
                     <p className="counter-btn" onClick={() => onRemove(item)}>-</p>
