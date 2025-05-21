637486f526353bdc0309097bbc61a6739bb22be1
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-21 20:15:19 +0300

    Fixed search function

--- Library/Common/Search.cs
+++ Library/Common/Search.cs
@@ -5,63 +5,64 @@ namespace Library.Common
 {
     public static class Search
     {
-        public static IQueryable<T> SearchBy<T>(this IQueryable<T> query, string searchTerm,
-            params Expression<Func<T, string?>>[] searchFields)
-        {
-            if (string.IsNullOrWhiteSpace(searchTerm) || searchFields.Length == 0)
-                return query;
-
-            searchTerm = $"%{searchTerm.ToLower()}%";
-
-            var parameter = Expression.Parameter(typeof(T), "x");
-
-            Expression? predicate = null;
-
-            foreach (var field in searchFields)
-            {
-                /*var property = Expression.Invoke(field, parameter);
-                var toLowerMethod = typeof(string).GetMethod("ToLower", Type.EmptyTypes);
-                var containsMethod =
-                    typeof(DbFunctionsExtensions).GetMethod("Like",
-                        [typeof(DbFunctions), typeof(string), typeof(string)]);
-
-                if (toLowerMethod != null && containsMethod != null)
+        /*        public static IQueryable<T> SearchBy<T>(this IQueryable<T> query, string searchTerm,
+                    params Expression<Func<T, string?>>[] searchFields)
                 {
-                    var toLowerExpression = Expression.Call(property, toLowerMethod);
-                    var likeExpression = Expression.Call(null, containsMethod, Expression.Constant(EF.Functions),
-                        toLowerExpression, Expression.Constant(searchTerm));
+                    if (string.IsNullOrWhiteSpace(searchTerm) || searchFields.Length == 0)
+                        return query;
 
-                    predicate = predicate == null ? likeExpression : Expression.OrElse(predicate, likeExpression);
-                }*/
-                // Access the property directly (MemberExpression)
-                var member = ReplaceParameter(field.Body, field.Parameters[0], parameter);
+                    searchTerm = $"%{searchTerm.ToLower()}%";
 
-                var toLower = Expression.Call(member,
-                    typeof(string).GetMethod(nameof(string.ToLower), Type.EmptyTypes)!);
+                    var parameter = Expression.Parameter(typeof(T), "x");
 
-                var likeMethod = typeof(DbFunctionsExtensions).GetMethod(
-                    nameof(DbFunctionsExtensions.Like),
-                    new[] { typeof(DbFunctions), typeof(string), typeof(string) }
-                )!;
+                    Expression? predicate = null;
 
-                var likeCall = Expression.Call(
-                    null,
-                    likeMethod,
-                    Expression.Constant(EF.Functions),
-                    toLower,
-                    Expression.Constant(searchTerm)
-                );
+                    foreach (var field in searchFields)
+                    {
+                        *//*var property = Expression.Invoke(field, parameter);
+                        var toLowerMethod = typeof(string).GetMethod("ToLower", Type.EmptyTypes);
+                        var containsMethod =
+                            typeof(DbFunctionsExtensions).GetMethod("Like",
+                                [typeof(DbFunctions), typeof(string), typeof(string)]);
 
-                predicate = predicate == null ? likeCall : Expression.OrElse(predicate, likeCall);
-            }
+                        if (toLowerMethod != null && containsMethod != null)
+                        {
+                            var toLowerExpression = Expression.Call(property, toLowerMethod);
+                            var likeExpression = Expression.Call(null, containsMethod, Expression.Constant(EF.Functions),
+                                toLowerExpression, Expression.Constant(searchTerm));
 
-            /*if (predicate == null)
-                return query;*/
-            /*var lambda = Expression.Lambda<Func<T, bool>>(predicate, parameter);
-            return query.Where(lambda);*/
+                            predicate = predicate == null ? likeExpression : Expression.OrElse(predicate, likeExpression);
+                        }*//*
+                        // Access the property directly (MemberExpression)
+                        var member = ReplaceParameter(field.Body, field.Parameters[0], parameter);
 
-            return query.Where(Expression.Lambda<Func<T, bool>>(predicate!, parameter));
-        }
+                        var toLower = Expression.Call(member,
+                            typeof(string).GetMethod(nameof(string.ToLower), Type.EmptyTypes)!);
+
+                        var likeMethod = typeof(DbFunctionsExtensions).GetMethod(
+                            nameof(DbFunctionsExtensions.Like),
+                            new[] { typeof(DbFunctions), typeof(string), typeof(string) }
+                        )!;
+
+                        var likeCall = Expression.Call(
+                            null,
+                            likeMethod,
+                            Expression.Constant(EF.Functions),
+                            toLower,
+                            Expression.Constant(searchTerm)
+                        );
+
+                        predicate = predicate == null ? likeCall : Expression.OrElse(predicate, likeCall);
+                    }
+
+                    *//*if (predicate == null)
+                        return query;*/
+        /*var lambda = Expression.Lambda<Func<T, bool>>(predicate, parameter);
+        return query.Where(lambda);*//*
+
+        return query.Where(Expression.Lambda<Func<T, bool>>(predicate!, parameter));
+    }*/
+
 
         public static IEnumerable<T> InMemorySearch<T>(this IEnumerable<T> source, string searchTerm,
             params Func<T, string?>[] fields)
@@ -78,7 +79,7 @@ namespace Library.Common
                     return value != null && value.Contains(searchTerm, StringComparison.CurrentCultureIgnoreCase);
                 }));
         }
-        
+
         // Replaces all instances of `source` in `expression` with `target`
         private static Expression ReplaceParameter(Expression expression, ParameterExpression source, Expression target)
         {
