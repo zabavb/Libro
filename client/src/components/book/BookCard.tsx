114372c7a61203b98b62cb9136c0fa256453da07
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-05-24 00:53:11 +0300

    book card update

--- client/src/components/book/BookCard.tsx
+++ client/src/components/book/BookCard.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BookView } from "../../types"
 import "react-lazy-load-image-component/src/effects/blur.css"
-import { likeBook, unlikeBook, isBookLiked, } from "../../services/likedBooksStorage";
 import { CartItem } from "@/types/types/cart/CartItem"
 import "@/assets/styles/components/book/card.css"
 import { icons } from '@/lib/icons';
-
+import noImageUrl from '@/assets/noImage.svg'
 interface BookAdminCardProps {
     book: BookView
     onNavigate: () => void
@@ -13,21 +12,7 @@ interface BookAdminCardProps {
 }
 
 const BookCard: React.FC<BookAdminCardProps> = ({ onAddItem, book, onNavigate }) => {
-    const [liked, setLiked] = useState(false);
 
-    useEffect(() => {
-        setLiked(isBookLiked(book.bookId));
-    }, [book.bookId]);
-
-    const toggleLike = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        if (liked) {
-            unlikeBook(book.bookId);
-        } else {
-            likeBook(book);
-        }
-        setLiked(!liked);
-    };
 
     return (
         <div className="card-container">
@@ -36,7 +21,7 @@ const BookCard: React.FC<BookAdminCardProps> = ({ onAddItem, book, onNavigate })
                         e.stopPropagation()
                         onNavigate()
                     }}
-            src={book.imageUrl ? book.imageUrl :`https://picsum.photos/seed/${book.bookId}/220/340`} />
+            src={book.imageUrl ? book.imageUrl : noImageUrl} />
             <div>
                 <div 
                     className="card-info">
@@ -51,15 +36,16 @@ const BookCard: React.FC<BookAdminCardProps> = ({ onAddItem, book, onNavigate })
                         AUTHOR_TMP
                     </p>
                 </div>
+
             </div>
             <div className="card-footer">
                 <div>
-                <p className="book-price">{book.price} UAH</p>
+                <p className="book-price">{book.price.toFixed(2)} UAH</p>
                 {book.quantity > 0 ? (<p className="book-availability">Available</p>) : (<p className="book-availability">Not Available</p>)}
                 </div>
                 <button
                     onClick={() => onAddItem({ bookId: book.bookId, amount: 1, name: book.title, price: book.price })}
-                    className="book-cart-btn">
+                    className="book-cart-btn min-w-10 min-h-10">
                     <img src={icons.bCart}></img>
                 </button>
             </div>
